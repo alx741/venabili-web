0d2e1e1987ff9bd34eff0daf4af2722eb3c23c16
Daniel Campoverde <alx@sillybytes.net>
2017-12-02 13:35:02 -0500

    Output the site to 'docs'

--- site.hs
+++ site.hs
@@ -3,7 +3,7 @@ import           Data.Monoid (mappend)
 import           Hakyll
 
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -73,3 +73,6 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+config :: Configuration
+config = defaultConfiguration { destinationDirectory = "docs" }
